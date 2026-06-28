b4f0c643ca1996930009f0232fc85a80a0134c17
Brandon Leon <brandonleon488@gmail.com>
2026-06-27 17:39:14 -0700

    cleanup temp files

--- fix_dash.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-api_code = open("src/api.py").read()
-if "/dashboard" not in api_code:
-    code = """
-"""
-    api_code = api_code.replace("if __name__", code + "\nif __name__")
-    pathlib.Path("src/api.py").write_text(api_code)
-    print("OK")
